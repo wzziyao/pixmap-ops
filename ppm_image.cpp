cb0aeb5f8c69e96a0a185d32ee4a680863be3b0f
wzziyao <ziyaow36@gmail.com>
2021-03-01 22:36:26 -0500

    write load, save, width, height, get, set functions

--- ppm_image.cpp
+++ ppm_image.cpp
@@ -1,6 +1,7 @@
 #include "ppm_image.h"
 #include <string>
 #include <fstream>
+#include <sstream> 
 
 using namespace agl;
 using namespace std;
@@ -35,12 +36,49 @@ ppm_image::~ppm_image()
 
 bool ppm_image::load(const std::string& filename)
 {
-   return false;
+   ifstream file(filename);
+
+   if(!file) {
+      file.close();
+      return false;
+   } else {
+      string w_string;
+      string h_string;
+      file >> format; 
+      file >> w_string; // width
+      file >> h_string; // height
+      file >> max_color_val; 
+
+      stringstream width(w_string);
+      stringstream height(h_string);
+      width >> w;
+      height >> h;
+      int size = w * h;
+      for(int i = 0; i < size; i++) {
+         unsigned char r = 0;
+         unsigned char g = 0;
+         unsigned char b = 0;
+         file >> r;
+         file >> g;
+         file >> b;
+         ppm_pixel next = {r, g, b};
+         colors[i] = next;
+      }
+      file.close();
+      return true;
+   }
 }
 
 bool ppm_image::save(const std::string& filename) const
 {
-   return false;
+   fstream fileStream;
+   fileStream.open(filename);
+
+   if (fileStream.fail()) {
+      return false;
+   } else {
+      return true;
+   }
 }
 
  ppm_image ppm_image::resize(int w, int h) const
@@ -85,19 +123,26 @@ ppm_image ppm_image::grayscale() const
 
 ppm_pixel ppm_image::get(int row, int col) const
 {
-   return ppm_pixel{ 0,0,0 };
+   if(row <= h && col <= w) {
+      return colors[row * w + col];
+   } else {
+      cout << "Please enter valid row and column numbers!" << endl;
+      ppm_pixel empty = {0, 0, 0};
+      return empty;
+   }
 }
 
 void ppm_image::set(int row, int col, const ppm_pixel& c)
 {
+   colors[row * w + col] = c;
 }
 
 int ppm_image::height() const
 {
-   return 0;
+   return h;
 }
 
 int ppm_image::width() const
 {
-   return 0;
+   return w;
 }
